6a208b54bb8fb5ccf372923a7839ed8c47dc8040
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-11-17 02:38:53 +0900

    fixed pattern_scanning example
    pattern_scanning example is bunch of personal test codes that i used in developing process. deleted it

--- examples/pattern_scanning.rs
+++ examples/pattern_scanning.rs
@@ -11,15 +11,21 @@ toy_arms::create_entrypoint!(hack_main_thread);
 fn hack_main_thread() {
     let memory = Module::from_module_name("client.dll").unwrap();
     let dw_force_attack_pattern = "89 0D ? ? ? ? 8B 0D ? ? ? ? 8B F2 8B C1 83 CE 04";
+
+    match memory.find_pattern(dw_force_attack_pattern) {
+        Some(i) => println!("offset: 0x{:x}", i),
+        None => println!("Pattern not found")
+    }
+
     match pattern_scan_all_modules(dw_force_attack_pattern, 2, 0) {
-        Ok(e) => println!("result: {:x}", e.0),
+        Ok(e) => {
+            println!("RESULT");
+            println!("address: {:x}", e.0);
+            println!("module name: {}", e.1);
+        },
         _ => println!("Not Found")
     }
 
-    //match memory.find_pattern(dw_force_attack_pattern) {
-    //    Some(i) => println!("offset: 0x{:x}", i),
-    //    None => println!("Pattern not found")
-    //}
     loop {
         // To exit this hack loop when you input INSEERT KEY
         if detect_keydown(VirtualKeyCode::VK_INSERT) {
